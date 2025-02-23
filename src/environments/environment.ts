4ec79e68ecaea2925e41caf25ae32a860cfefae9
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2025-02-23 23:24:04 +0500

    update API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
 	production: false,
-	apiBaseUrl: "http://localhost:3000",
+	apiBaseUrl: "http://localhost:8080",
 	CAPACITOR_ANDROID_STUDIO_PATH: "/opt/android-studio-2024.1.2/android-studio/bin/studio.sh",
 	YandexMapKey: "e1af837b-87eb-4521-9ee9-7f99139464ed",
 	RouteKey: "5b3ce3597851110001cf6248ef4c3cd6319742bea51882e8e0124440",
